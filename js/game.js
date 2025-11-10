ab265c50cd4bdf6f8f7908ea95ee0a522e1a0792
Mateo <mateyro999@gmail.com>
2025-11-09 23:23:38 -0300

    anotaciones en el codigo de game.js

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 class TableroDinosaurios {
   constructor() {
     this.jugadas = JSON.parse(localStorage.getItem("jugadas")) || [];
+    //usado para generar dinosaurios
     this.listaDinos = [
       "T-Rex",
       "Velociraptor",
@@ -9,8 +10,9 @@ class TableroDinosaurios {
       "Spinosaurio",
       "Anquilosaurio"
     ];
-    this.dinosActuales = JSON.parse(localStorage.getItem("dinosActuales")) || []; // dinos visibles
+    this.dinosActuales = JSON.parse(localStorage.getItem("dinosActuales")) || [];
     
+    //esto es para que me acuerde yo, esto es como un if, la estructura es asi. condition ? valueIfTrue : valueIfFalse
     this.reposicionesRestantes = localStorage.getItem("reposicionesRestantes") !== null
     ? parseInt(localStorage.getItem("reposicionesRestantes"))
     : 6;
@@ -29,6 +31,7 @@ class TableroDinosaurios {
     this.agregarEventoFinalizar();
   }
 
+  //tomo un rato, borre y reescribi el codigo unas cuantas veces hasta que encontre como hacerlo
    generarDinosauriosAleatorios() {
     const contenedor = document.querySelector(".dinos-container");
     contenedor.innerHTML = "";
@@ -36,6 +39,7 @@ class TableroDinosaurios {
 
     for (let i = 0; i < 6; i++) {
         const dino = this.crearDinosaurio(contenedor);
+        //push, metodo de js, agrega un nuevo elemento al final de un array
         this.dinosActuales.push(dino);
     }
     this.agregarEventosDrag();
@@ -63,6 +67,9 @@ class TableroDinosaurios {
     const randomIndex = Math.floor(Math.random() * this.listaDinos.length);
     const dinoNombre = this.listaDinos[randomIndex];
     const emoji = dinoNombre === "T-Rex" ? "🦖" : "🦕";
+    //no sabia como lidiar con IDs entonces checkee stack overflow y me encontre con esto
+    //para explicar, toString(36) lo convierte a base 36 que es un tipo de encripcion, y el substring(2, 9)
+    //hace que se remuevan unos simbolos que siempre aparecen en base 36, "0."
     const id = `dino-${Math.random().toString(36).substring(2, 9)}`;
     const texto = `${emoji} ${dinoNombre}`;
 
@@ -83,15 +90,17 @@ class TableroDinosaurios {
       const contenedor = document.querySelector(".dinos-container");
       const nuevoDino = this.crearDinosaurio(contenedor);
       this.dinosActuales.push(nuevoDino);
+      //no fue divertido trabajar con funciones en funciones
       this.agregarEventosDrag();
       this.reposicionesRestantes--;
-      console.log("Reposición! Restan:", this.reposicionesRestantes);
+      console.log("Reposicion. Quedan:", this.reposicionesRestantes);
       this.guardarEstadoDinos();
     } else {
-      console.log("No quedan más dinosaurios para reponer.");
+      console.log("No quedan mas dinosaurios.");
     }
   }
 
+  //probablemente lo mas simple que e tenido que hacer fue editar esto
   guardarEstadoDinos() {
     localStorage.setItem("dinosActuales", JSON.stringify(this.dinosActuales));
     localStorage.setItem(
@@ -111,6 +120,8 @@ class TableroDinosaurios {
           casilla.textContent = casilla.dataset.original + "\n";
           casilla.dataset.cargada = "true";
         }
+        //me acorde de /n de primero de utu y la verdad es la razon entera por la cual e podido
+        //hacer lo de mostrar multiples dinosaurios
         casilla.textContent += `🦕 ${jugada.dinosaurio}\n`;
         }
       });
@@ -123,7 +134,8 @@ class TableroDinosaurios {
     console.log("Jugadas:", this.jugadas);
   }
 
-  resetearJugadas() {
+    //boton de finalizar temporal
+    resetearJugadas() {
     localStorage.removeItem("jugadas");
     localStorage.removeItem("dinosActuales");
     localStorage.removeItem("reposicionesRestantes");
@@ -147,6 +159,7 @@ class TableroDinosaurios {
     });
   }
 
+  //esto fue una pesidilla para lidiar con
   agregarEventosDrop() {
     document.querySelectorAll(".casilla").forEach((casilla) => {
       casilla.addEventListener("dragover", (e) => e.preventDefault());
@@ -157,7 +170,10 @@ class TableroDinosaurios {
         const dino = e.dataTransfer.getData("text/plain");
         const idCasilla = casilla.dataset.casilla;
         const limite = parseInt(casilla.dataset.limite) || 1;
-
+        
+        //mi mas humilde manera de encontrar cuantos dinosaurios hay en la casilla. filter filtra
+        //aqui por todas las jugadas intentando encontrar jugadas las cuales tengan la id de casilla
+        //igual a la nuestra
         const cantidadActual = this.jugadas.filter(
             (j) => j.casilla === idCasilla
         ).length;
@@ -172,6 +188,8 @@ class TableroDinosaurios {
 
         this.guardarJugadas();
 
+        //datatransfer y todo eso es parte del dragstart event listener viene por defecto
+        //simplemente obtiene y guarda el id del dinosaurio
         const dinoId = e.dataTransfer.getData("id");
         const dinoElemento = document.querySelector(`.dino[data-id="${dinoId}"]`);
         if (dinoElemento) {
